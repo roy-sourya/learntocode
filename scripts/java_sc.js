2e142cab78dcf3cbb492f89010d078b9ff61e040
roy-sourya <57309260+roy-sourya@users.noreply.github.com>
2020-03-24 16:17:56 +0530

    Second commit redone

--- scripts/java_sc.js
+++ scripts/java_sc.js
@@ -1,7 +1,12 @@
 var link1=document.getElementById("link_fb_em");
+var link0=document.getElementById("link_fb_root");
 var link2=document.getElementById("link_fb_mz");
 var link3=document.getElementById("link_fb_bg");
 var link4=document.getElementById("link_fb_sp");
+
+link0.addEventListener("click", function(){
+    window.open('https://www.facebook.com/sourya.roy.161','_blank');
+});
 
 link2.addEventListener("click", function(){
     window.open('https://www.facebook.com/zuck', '_blank');
@@ -10,8 +15,8 @@ link1.addEventListener("click", function(){
     window.open('https://www.facebook.com/TeslaMoto/','_blank');
 });
 link3.addEventListener("click", function(){
-    window.location.href = 'https://www.facebook.com/zuck';
+    window.open('https://www.facebook.com/BillGates/','_blank');
 });
 link4.addEventListener("click", function(){
-    window.location.href = 'https://www.facebook.com/zuck';
+    window.open('https://www.facebook.com/Sundar-pichai-followers-470769226664823/','_blank');
 });
