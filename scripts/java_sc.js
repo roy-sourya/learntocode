4550e0061c5d662ea739819783159008e6db713c
roy-sourya <roysourya10@iitkgp.ac.in>
2020-03-24 15:10:03 +0530

    First commit

---
+++ scripts/java_sc.js
@@ -0,0 +1,17 @@
+var link1=document.getElementById("link_fb_em");
+var link2=document.getElementById("link_fb_mz");
+var link3=document.getElementById("link_fb_bg");
+var link4=document.getElementById("link_fb_sp");
+
+link2.addEventListener("click", function(){
+    window.open('https://www.facebook.com/zuck', '_blank');
+});
+link1.addEventListener("click", function(){
+    window.open('https://www.facebook.com/TeslaMoto/','_blank');
+});
+link3.addEventListener("click", function(){
+    window.location.href = 'https://www.facebook.com/zuck';
+});
+link4.addEventListener("click", function(){
+    window.location.href = 'https://www.facebook.com/zuck';
+});
